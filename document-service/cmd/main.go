738a2b3b19a488e2044bf7151b16ecc8d5bbf319
BHAV0207 <bhavyajain4274@gmail.com>
2025-11-19 01:13:04 +0530

    added complete backend layer for broadcasting the messages

--- document-service/cmd/main.go
+++ document-service/cmd/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/BHAV0207/documet-service/internal/websockets"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("no env found")
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "7000"
+	}
+
+	hub := websockets.NewHub()
+	go hub.Run()
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/ws/document/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		DocID := vars["id"]
+
+		websockets.ServerWs(hub, w, r, DocID)
+	})
+
+	if err := http.ListenAndServe(PORT, r); err != nil {
+		log.Fatal("Server Error:", err)
+	}
+
+}
