5809f2b3f3e00c393405fcbfe2db8db4e00cf14a
BHAV0207 <bhavyajain4274@gmail.com>
2025-11-22 15:55:56 +0530

    Added the handlers

--- AI-service/go.mod
+++ AI-service/go.mod
@@ -7,8 +7,11 @@ toolchain go1.24.10
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
 	github.com/gofiber/fiber/v2 v2.52.10 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
---
+++ AI-service/internal/handler/ai.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/BHAV0207/AI-service/internal/services"
+	"github.com/gofiber/fiber"
+)
+
+type AIHandler struct {
+	LLM *services.LLMService
+}
+
+func NewAiHandler(llm *services.LLMService) *AIHandler {
+	return &AIHandler{LLM: llm}
+}
+
+func (h *AIHandler) Summarize(c *fiber.Ctx) error {
+	var body struct {
+		Text string `json:"text"`
+	}
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Invalid request body",
+		})
+	}
+
+	res, err := h.LLM.Summarize(context.Background(), body.Text)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+	return c.JSON(fiber.Map{
+		"summary": res,
+	})
+}
+
+func (h *AIHandler) Suggest(c *fiber.Ctx) error {
+	var body struct {
+		Context string `json:"context"`
+	}
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"error": "Invalid request body",
+		})
+	}
+
+	res, err := h.LLM.Suggest(context.Background(), body.Context)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"suggestions": res,
+	})
+}
